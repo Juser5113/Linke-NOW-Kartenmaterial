acb686a6193e7d836a3a6212a38d7ce3da1e5fe8
Juser5113 <github@juser5113.it>
2026-07-16 23:19:44 +0200

    docs(html): normalize markup formatting in index.html

---
+++ js/config/uebersicht.js
@@ -0,0 +1,9 @@
+﻿// Config für index.html – Zoom/Center hier anpassen.
+// Nach jeder Änderung die Versionsnummer im <script>-Tag von index.html
+// hochzählen (z.B. ?v=1 -> ?v=2), sonst liefert der Browser evtl. die
+// gecachte alte Version aus.
+initKartenseite({
+    geojson: 'data/uebersicht.geojson',
+    center: [52.541, 13.567],
+    zoom: 14
+});
